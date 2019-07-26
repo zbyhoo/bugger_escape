b4e9017ab4cfc3438de5a2355e615ebf471af066
zbyhoo <zbyhoo@gmail.com>
2019-07-26 11:35:50 +0200

    first

---
+++ Assets/Scripts/Acceleration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acceleration : MonoBehaviour
+{
+    [SerializeField] private float _targetSpeed;
+    [SerializeField] private float _forceMult;
+    [SerializeField] private float _forceMax;
+
+    [SerializeField] private Rigidbody2D _body;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float force = (_targetSpeed - _body.velocity.x) * _forceMult;
+
+        if (force > _forceMax)
+        {
+            force = _forceMax;
+        }
+        
+        _body.AddForce(new Vector2(force, 0));
+    }
+}
---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private Transform _cameraTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraTransform = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _cameraTransform.position = new Vector3(_target.position.x, _cameraTransform.position.y, _cameraTransform.position.z);
+    }
+}
