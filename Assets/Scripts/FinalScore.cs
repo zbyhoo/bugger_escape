2cf57f7ae215da0f889a4dc096b2764ce091947f
zbyhoo <zbyhoo@gmail.com>
2019-07-26 16:46:52 +0200

    adding scroing at the end

---
+++ Assets/Scripts/FinalScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using TMPro;
+using UnityEngine;
+
+public class FinalScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI label = gameObject.GetComponent<TextMeshProUGUI>();
+        label.text = "SCORE: " + Scoring.GetPoints();
+    }
+}
