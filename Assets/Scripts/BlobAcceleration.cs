a5b3da195ff1351cac5710679ab0955f8af5b784
zbyhoo <zbyhoo@gmail.com>
2019-07-26 12:11:39 +0200

    blob

---
+++ Assets/Scripts/BlobAcceleration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobAcceleration : MonoBehaviour
+{
+    [SerializeField] private float _targetSpeed;
+    [SerializeField] private float _forceMult;
+    [SerializeField] private float _forceMax;
+
+    [SerializeField] private UnityJellySprite _body;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float force = (_targetSpeed - _body.GetCentralPointVelocity().x) * _forceMult;
+
+        if (force > _forceMax)
+        {
+            force = _forceMax;
+        }
+        
+        _body.AddForce(new Vector2(force, 0));
+    }
+}
---
+++ Assets/Scripts/BlobGravitySwitch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobGravitySwitch : MonoBehaviour
+{
+    [SerializeField] private UnityJellySprite _body;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _body.SwitchGravity();
+        }
+    }
+}
